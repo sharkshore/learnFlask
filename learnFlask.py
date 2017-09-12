8b4a0a4349098ba0a6a6709b60f399619b108511
shark <sharkshore@163.com>
2017-09-12 14:30:49 +0800

    学习基础

--- learnFlask.py
+++ learnFlask.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, url_for
+from flask import Flask, request, url_for, render_template
 
 app = Flask(__name__)
 
@@ -15,12 +15,25 @@ def hello():
 def show_user_profile(username):
 	return 'User %s' % username
 
+#判断get请求,还是post请求
 @app.route('/login',methods=['GET','POST'])
 def login():
 	if request.method == 'POST':
 		return '登录成功'
 	else:
 		return '登录失败'
+
+@app.route('/hellohtml')
+@app.route('/hellohtml/<name>')
+def hellohtml(name=None):
+	app.logger.info('获取了一个请求',hellohtml)
+	return render_template('hello.html',name=name)
+
+#获取get请求参数
+@app.route('/param')
+def param():
+	return request.args.get('name')
+
 
 
 if __name__ == '__main__':
